055debe51bd6f02a4244a577937d3607ea471f3d
Tahira <tahiraqadir29@gmail.com>
2023-12-19 20:23:23 +0500

    add the manu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-import { SearchIcon } from '@heroicons/react/outline'
+import { HomeIcon, PlusCircleIcon, SearchIcon } from '@heroicons/react/outline'
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto mt-4">
       {/* Left */}
         <div className="h-24 w-24 cursor-pointer relative hidden lg:inline-grid">
             <Image src="/logo.png" alt="Intagram-logo-1" layout="fill" className="object-contain" />
@@ -23,7 +24,14 @@ export default function Header() {
 
       {/* Right */}
 
-      <h1>Right Side</h1>
+      <div>
+        <div className="flex space-x-5 items-center">
+        <HomeIcon className="h-6 hidden md:inline-flex cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img src="/profile.png" alt="Intagram-logo-2" className="h-10 cursor-pointer" />
+        </div>
+      </div>
+
     </div>
   )
 }
