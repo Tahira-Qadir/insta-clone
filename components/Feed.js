2a05f736bdf9b35a916ae3e727daded133d6f39d
Tahira <tahiraqadir29@gmail.com>
2023-12-20 12:06:58 +0500

    build the suggestion part of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,7 @@
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestion from "./Suggestion";
 
 export default function Feed() {
   return (
@@ -22,8 +23,8 @@ export default function Feed() {
           {/* Mini Profile */}
           <MiniProfile />
           {/* Suggestion */}
+          <Suggestion />
         </div>
-
       </section>
     </main>
   )
