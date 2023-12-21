a25ebcecdeb11590991d523564a46208f8612b66
Tahira <tahiraqadir29@gmail.com>
2023-12-21 15:44:10 +0500

    apply like functionality

--- components/Post.js
+++ components/Post.js
@@ -2,24 +2,44 @@
 
 import { db } from "@/firebase";
 import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon } from "@heroicons/react/outline";
-import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import Moment from "react-moment";
+import { HeartIcon as HeartIconfilled } from "@heroicons/react/solid"
 
 
 export default function Post({img, userImg, caption, userName, id}) {
   const {data: session} = useSession()
   const [comment, setComment] = useState("")
-
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
   const [comments, setComments] = useState([])
   useEffect(() => {
-    const unSubscribe = onSnapshot(
+    const unsubscribe = onSnapshot(
       query(collection(db, "posts", id,  "comments"), orderBy("timestamp", "desc")), 
       (snapshot) =>{setComments(snapshot.docs)}
     )
   }, [id])
 
+  useEffect(() =>{
+    const unsubscribe = onSnapshot(
+      collection(db, 'posts', id,  'likes'), (snapshot) =>setLikes(snapshot.docs)
+    )
+  }, )
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+    }, [likes, session?.user.uid]);
+
+  async function likePost(){ 
+    if(hasLiked){
+      await deleteDoc(doc(db,'posts',id,'likes',session.user.uid))
+    }
+    await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+      username: session.user.username,
+    })
+  }
+  
   async function sendComment(event){
     event.preventDefault();
     const commentToSend = comment;
@@ -55,7 +75,8 @@ export default function Post({img, userImg, caption, userName, id}) {
        {session && (
         <div className="flex justify-between px-4 pt-4">
             <div className="flex space-x-4">
-                <HeartIcon className="btn" />
+            {hasLiked ? (<HeartIconfilled onClick={likePost} className='text-red-400 btn' />): 
+            <HeartIcon onClick={likePost} className='btn' />}
                 <ChatIcon className="btn" />
             </div>
             <BookmarkIcon className="btn" />
